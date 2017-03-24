70c95921be6b7f3ea65cf9c1c91a487e8003f6eb
philip <philip.oswald@gmail.com>
2017-03-24 21:00:14 +0100

    Implement custom LogManager to allow logging during shutdown

--- src/main/java/styx/daemon/utils/LogConfig.java
+++ src/main/java/styx/daemon/utils/LogConfig.java
@@ -11,8 +11,20 @@ import java.util.logging.LogManager;
  * <pre>
  * [2017-03-23 23:09:53.257] [INFO   ] styx.http.server.Server --- Started HTTP server on port 8080
  * </pre>
+ * <p>
+ * Additionally, this class installs a custom LogManager, which disables the reset() method.
+ * This ensures that logging remains possible while JVM shutdown hooks are being executed.
+ * Caveats:
+ * <ul>
+ * <li> calling reset() has no effect
+ * <li> handlers are not closed before exitting
+ * </ul>
  */
 public class LogConfig {
+
+    static {
+        System.setProperty("java.util.logging.manager", CustomLogManager.class.getName());
+    }
 
     public static void activate() {
         activate("/styx.logging.properties");
@@ -26,6 +38,13 @@ public class LogConfig {
             LogManager.getLogManager().readConfiguration(stream);
         } catch (IOException | SecurityException e) {
             throw new IllegalStateException("Failed to initialize java.util.logging.", e);
+        }
+    }
+
+    public static class CustomLogManager extends LogManager {
+        @Override
+        public void reset() {
+            /* do NOT call superclass, disable shutdown hook */
         }
     }
 }
